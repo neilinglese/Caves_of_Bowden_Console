0d7b3a3ba0b552c08c3d7844fc3525d8a8438d75
Neil Inglese <neilinglese@gmail.com>
2014-05-19 15:09:06 -0500

    cleaned up Program.cs

--- CavesofBowden/Program.cs
+++ CavesofBowden/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using CavesofBowden;
 
-namespace frames
+namespace CavesofBowden
 {
     class Program
     {
@@ -156,75 +156,4 @@ namespace frames
         }
     }//title,intro,varables
    
-    class Stats //stats
-    {
-        public Stats(string[] names, string[] menu, string[] text, bool[] item, int[] num)
-        {
-            Console.Clear();
-            Console.WriteLine("Name: " + names[0]);
-            Console.WriteLine("Class: " + names[1]);
-            Console.WriteLine("========================");
-            Console.WriteLine("Health: " + num[0]);
-            Console.WriteLine("Willpower: " + num[1]);
-            Console.WriteLine("Strength: " + num[2]);
-            Console.WriteLine("Agility: " + num[3]);
-            Console.WriteLine("Luck: " + num[4]);
-            Console.WriteLine("\n\n");
-            Console.WriteLine("Party Members");
-            Console.WriteLine("========================");
-            if (item[0] == true)
-            {
-                Console.WriteLine(names[3]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-            if (item[1] == true)
-            {
-                Console.WriteLine(names[4]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-            if (item[2] == true)
-            {
-                Console.WriteLine(names[5]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-
-            Console.WriteLine("\nInventory");
-            Console.WriteLine("========================");
-            if (item[3] == true)
-            {
-                Console.WriteLine(names[6]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-            if (item[4] == true)
-            {
-                Console.WriteLine(names[7]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-            if (item[5] == true)
-            {
-                Console.WriteLine(names[8]);
-            }
-            else
-            {
-                Console.WriteLine(names[14]);
-            }
-            Console.ReadLine();
-            Console.Clear();
-        }
-    }
 }
